e1e1ea232e81b1d05b87b29e37764625ffbf13fb
Owen Ying <495711146@qq.com>
2019-02-06 12:43:15 -0500

    update

--- MathWorks/src/Resume/SupermarketDiscount.java
+++ MathWorks/src/Resume/SupermarketDiscount.java
@@ -35,55 +35,26 @@ OOD:
         |--Discount
         |--Product
         |--Market
-        |--
-
-        |--profile image path
-        |--course primary key
-    |--Student Performance Data
+        |--Market Address
+    |--Area
         |--primary key
-        |--performance JSON
-        |--student primary key: foreign key
+        |--Area Name
 
-Front-end: html + css + JQuery
-    I. Jquery:
-        1. Event based
-        2. Five levels: Has lots of duplicate codes, but since each of them are slightly different, hard to encapsulate them
-           Especially the events, different interaction with different css style, the problem is "if you use separate code for
-           each level, there are lots of duplicated codes, but if you encapsulate them, the parameters are too many and there are
-           too many if-statement"
-        3. Code granularity Trade-off:
-            |--If each level in single javascript:
-                |--Cons: the javascript file is very long;  hard to be reused.
-                |--Pros: variable passing efficiently; reduce backend workload; reduce front-back communication;
-                         reduce waiting time, increase game speed
-            |--If each scene in single javascript
-                |--Cons: frequent interaction with backend; long waiting time; high backend workload; inefficient variable passing
-                |--Pros: easy to reuse each scene; short javascript file; easy to maintain
-        4. Use JQery to add CSS and html code, violates encapsulation and make it hard to maintenance. But easy to dynamically
-           modify the styles and html elements.
+Front-end: JSP + css + Javascript
+    I. JSP:
+        1. <form action="GoodsAdd" method="POST">
 
-    II. CSS:
-        1. Design like tree structure, general class as root, specific class as leaves
-        2. GameFrame -- relative, Others: absolute, only can be played on 1024 supportive device.
 
-    III. html:
-        1. Use templates to reuse the code
-
-Back-end: Beans + mySQL + JSON
+Back-end: mySQL + MyEclipse
     I. Beans:
-        1. MVC model: Model-EntityBeans, View-HTML/CSS, Controller-ManagedBeans
-        2. EntityBeans --> FacadeBeans --> Controllers --> Managers;    Constants, Global Tools, Validators
-    II. mySQL + JSON:
-        1. Use mySQL to maintain standard database table formula
-        2. Use JSON stored in mySQL to add flexibility to the backend storage
-    III. Persistence API: Connect Beans and Databases
+        1. MVC model: Model-Domains, View-HTML/CSS, Controller-Services
+        2. DBUtil.getConnection() --> Dao(SQL) --> Domain(Entities) --> service --> servlet(accept requests)
 
 Front-end Back-end communication:
     I. Front-end to Back-end:
-        |--JSF page to Beans: Expression language
-        |--Javascript to Beans: hidden input form + JSON
+        |--doGet()
+        |--doPost()
     II. Back-end to Front-end:
-        |--Beans to JSF: Expression language
-        |--Beans to javascript: action listener
+        |--request.getSession().setAttribute("managerName", na);
 
  */
