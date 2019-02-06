7ca1edb9ad145005aaa0b39947f256d9cad2413e
OwenYing <495711146@qq.com>
2019-02-06 15:49:04 -0500

    update

--- MathWorks/src/WhyEDG.java
+++ MathWorks/src/WhyEDG.java
@@ -1,7 +1,31 @@
 public class WhyEDG {
 }
 /*
+Why EDG?
+    1. Help me transfer my knowledge from school to real world
+    2. I can interact with real users to know their needs
+    3. Solve real world problem
+    4. Learn lots of new knowledge and try every direction before jumping
+        into a specific team.
+    5. I will learn to use Matlab and Simulink taught by experts, and in the
+        future I can use Matlab to conduct amazing researches
+    6. I can work with really smart people from all around the world, working
+        with amazing people makes you improve faster.
+    7. I can keep learning, add skills to my skill set and make myself more
+        competitive.
 
+Why Fit?
+    1. My field now is Web Application development, which uses OOD a lot.
+        So I have strong foundation of OOD.
+    2. I am developing a full stack Web App all by myself, including front-end
+        and back-end. So I have foundation in both front-end and back-end, which makes
+        me easy to fit into wider range of projects.
+    3. I also have machine learning background, maybe in the future those knowledge
+        might works.
+    4. I love to communicate with people, and get to know what users requirement.
+        I'd love to help them with their real world problems.
+    5. And I'm eager to learn new things, the SlidesController project on my resume,
+        I used ten day developed it with no Android knowledge at the beginning.
 
 
 
