53b6e53e38e08f8234bef461b27ae0244d9364ca
OwenYing <495711146@qq.com>
2019-02-06 12:46:27 -0500

    update

---
+++ MathWorks/src/OA.java
@@ -0,0 +1,44 @@
+public class OA {
+}
+
+/*
+Math:
+1. 一个屋子里有400个人,他们之中有相同生日的概率?
+	Answer: 100%
+2. 52张牌中抽2张,这2张牌全是k的概率?
+	4/52 * 3/51 = 0.0045
+3*. 第一个袋子里有一个蓝球三个黄球, 第二个袋子里有三个蓝球两个黄球,从所有球中取出一个. 已知取的球是蓝色, 这个球来自第一个袋子的概率?
+	Conditional Probability. 3/8
+4*. n个点不成环,最多多少条边?
+	(n - 1)
+5?. 一个小镇，有tv的人家60个，有scooter的人家85个，有冰箱的70个，有radio set的95家，已知有130家只有其中一个物件，问这个小镇最多有几户人家？
+还有一个问signed integer能否表示1-10billion的数。
+6. 1. 5个蚂蚁在pentagon的顶点， 可以沿着边两头走， 问碰撞的几率多大？
+	1 - (2/2^5)
+7. drone 从 1 ave， 121 st出发到2ave， 1 st， 到了后发现东西掉了，问最有可能掉在哪里？
+8. 120 人选课， id偶数的选 a课， id%5==0 的选 b课， id%7 ==0 的选 c, 问多少人没选课
+	41
+9.一种tri bit， 能表达 0，1，2 问 8 位的tribit能最大 unsigned number
+	3^8-1
+10. 50 张牌， id 1到50，都扣着，第一次 都翻开，第二次 flip 2的倍数的牌， 第三次flip3的倍数。。。。。。第n次flip n的倍数的牌，50张最后几张正面？
+	7
+11. 淘汰赛，一共512人，要比几场？
+	511
+
+Coding:
+1. 给一个数组, 问最少经过多少次swap操作, 可使数组中所有奇数前, 所有偶数在后?   2 pointers
+	https://www.geeksforgeeks.org/segregate-even-and-odd-numbers/
+2. build offices -- leetcode 542
+3. Merge strings:
+	https://www.geeksforgeeks.org/alternatively-merge-two-strings-in-java/
+4. minimum difference of an array
+	https://www.geeksforgeeks.org/maximum-difference-between-two-elements/
+5?. The cost of a tree
+
+
+Video:
+1. what interests you about working in EDG
+2. what experience makes you a good fit for EDG
+3. 身份
+
+ */
