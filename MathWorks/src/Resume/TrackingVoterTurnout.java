7396a3a9c0ded03ac9ba563f4d3248d16b463aed
Owen Ying <495711146@qq.com>
2019-02-05 23:31:54 -0500

    update

---
+++ MathWorks/src/Resume/TrackingVoterTurnout.java
@@ -0,0 +1,25 @@
+package Resume;
+
+public class TrackingVoterTurnout {
+}
+
+/*
+OOD:
+    |--User
+    |--UserPhoto
+    |--Voters Info
+    |--Legislation
+
+Techs Used:
+    1. User login and create account
+    2. Send email of voting data
+    3. 2-factor email verification
+    4. Information visualization: PrimeFaces
+    5. Database search
+    6. Cookies to remember username and password
+    7. AES password encryption -- not very good, it can be decrypted
+    8. RSS news feeding
+    9. Recommend Youtube Videos: Youtube API
+
+
+ */
