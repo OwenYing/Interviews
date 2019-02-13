74b56e8ccb1c370893d21ca302119205c36021e5
OwenYing <495711146@qq.com>
2019-02-13 09:32:57 -0500

    update

--- MathWorks/src/WhyEDG.java
+++ MathWorks/src/WhyEDG.java
@@ -3,7 +3,7 @@ public class WhyEDG {
 /*
 Why EDG?
     1. Help me transfer my knowledge from school to real world
-    2. I can interact with real users to know their needs
+    2. I can interact with real users to know their needs, along the way I can practice communicating with people
     3. Solve real world problem
     4. Learn lots of new knowledge and try every direction before jumping
         into a specific team.
@@ -28,5 +28,31 @@ Why Fit?
         I used ten day developed it with no Android knowledge at the beginning.
 
 
+Teams:
+    1. Product Development
+        |--Design and develop software for MATLAB and Simulink and nearly 100 other products.
+        |--Create advanced computing features for data analysis, parallel computing, signal processing, algorithm development, embedded systems, code generation, control design automation, and more.
+        |--Specialize in domains such as computation, modeling and simulation, image and video processing, compiler technology, visualization, data analytics, machine learning, and more.
+    2. Web App and Services
+        |--Design and implement cutting-edge solutions for the Internet of Things (IoT), cloud, and mobile platforms.
+        |--Build and support our thriving online community where MATLAB and Simulink users pose challenging problems, get answers, and share knowledge.
+        |--Design and develop web services that deliver MathWorks products to customers worldwide.
+    3. Business Applications and Tools
+        |--Develop next-generation business applications and tools for MathWorks internal use.
+        |--Work on enterprise software applications for running the company.
+        |--Create web, mobile, desktop, and cloud computing applications.
+    4. Advanced Support
+        |--Work directly with customers in engineering and science around the world to resolve cases that require deep product knowledge or extensive industry experience.
+        |--Collaborate with product development engineers to resolve customer cases that require a change in core software products.
+        |--Provide voice-of-the-customer input to feature planning, usability, and other development activities by identifying emerging customer needs and extracting patterns in customer support requests.
+        |--Provide technical mentorship, training, and guidance to the Engineering Development Group.
+    5. Development University
+        |--Provide learning and training support for development teams.
+        |--Lead Lean process improvements throughout the development lifecycle.
+        |--Contribute to software projects from hands-on coding to testing and deployment.
+    6. Infrastructure and Architecture
+        |--Design and code software for building, testing, and releasing MathWorks products.
+        |--Design tools and functionality to enable the worldwide development organization to focus on creating great software.
+        |--Specialize in domains such as compilers, web development, databases, globalization, and more.
 
  */
