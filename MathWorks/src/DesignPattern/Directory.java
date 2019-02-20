ea76cec0b1a999a49958eb4287c2acc1739b2444
Owen Ying <495711146@qq.com>
2019-02-19 22:22:39 -0500

    update

--- MathWorks/src/DesignPattern/Directory.java
+++ MathWorks/src/DesignPattern/Directory.java
@@ -7,5 +7,5 @@ public class Directory {
 1. Abstract Factory -- Factory Maker
 2. Factory Method -- defer init to subclass
 3. Builder -- help to semantic constructor
-
+4. Facade -- provide easier surface of operating underneath complex operation
  */
