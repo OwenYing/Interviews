d3e2ac0293c63fece4ff0dde4c01bd1132cd052e
OwenYing <495711146@qq.com>
2019-02-20 10:33:55 -0500

    update

--- MathWorks/src/DesignPattern/Directory.java
+++ MathWorks/src/DesignPattern/Directory.java
@@ -8,4 +8,5 @@ public class Directory {
 2. Factory Method -- defer init to subclass
 3. Builder -- help to semantic constructor
 4. Facade -- provide easier surface of operating underneath complex operation
+5. Adapter -- for interface1 to fit interface2 ( implements interface2, while having interface1's instance)
  */
