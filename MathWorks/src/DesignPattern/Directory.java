fe8bb91a42c067271b52bc2f5b243aab84a3c7bd
OwenYing <495711146@qq.com>
2019-02-20 10:35:13 -0500

    update

--- MathWorks/src/DesignPattern/Directory.java
+++ MathWorks/src/DesignPattern/Directory.java
@@ -9,4 +9,9 @@ public class Directory {
 3. Builder -- help to semantic constructor
 4. Facade -- provide easier surface of operating underneath complex operation
 5. Adapter -- for interface1 to fit interface2 ( implements interface2, while having interface1's instance)
+
+
+
+
+
  */
